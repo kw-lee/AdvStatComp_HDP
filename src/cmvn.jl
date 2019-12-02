6ce1d29b7f6bf3755735b48b1666d20a5b695bac
Kyeongwon Lee <lkw1718@snu.ac.kr>
2019-12-02 21:10:13 +0900

    implement LDL()

--- src/cmvn.jl
+++ src/cmvn.jl
@@ -1,28 +1,40 @@
-using LinearAlgebra, Distributions, Statistics
+using LinearAlgebra, Distributions, Statistics, SparseArrays
 
 include("generate.jl")
 # include("truncnorm.jl")
 include("mvn.jl")
 
 """
+    LDL(A, d; m = size(Sigma, 1))
     Algorithm 1 in Cao2019
-    LDL decomposition
+    LDL decomposition: A = L * Matrix(D) * L'
+    input:
+        - A: positive definite matrix
+        - d: block size
+        - m: size of the matrices
+    output:
+        - L: LowerTriangular
+        - D: array{Matrix}
 """
-function LDL(Σ::Symmetric{T,Array{T,2}}, d::Int; m::Int = size(Σ, 1)) where T<:AbstractFloat
+function LDL(A::Symmetric{T,Array{T,2}}, d::Int; m::Int = size(Σ, 1)) where T<:AbstractFloat
     # m is multiple of d
     if m % d == 0
-        Σ = Matrix(Σ)
-        L = Matrix(1.0I, m, m)
-        D = zeros(m, m)
-        for i = 1:d:m-d+1
-            D[i:(i + d - 1), i:(i + d - 1)] .= Σ[i:(i + d - 1), i:(i + d - 1)]
-            L[(i + d):m, i:(i + d - 1)] .= Σ[(i + d):m, i:(i + d - 1)] * inv(D[i:(i + d - 1), i:(i + d - 1)])
+        Σ = Matrix(A)
+        L = Matrix(1.0LinearAlgebra.I(m))
+        D = Array{Matrix{T}}(undef, Int(m/d))
+        for j = 1:Int(m/d)
+            i = (j-1)*d + 1
+            D[j] = Σ[i:(i + d - 1), i:(i + d - 1)]
+            L[(i + d):m, i:(i + d - 1)] .= Σ[(i + d):m, i:(i + d - 1)] * inv(D[j])
             Σ[(i + d):m, (i + d):m] = Σ[(i + d):m, (i + d):m] .-
-                L[(i + d):m, i:(i + d - 1)] * D[i:(i + d - 1), i:(i + d - 1)] * transpose(L[(i + d):m, i:(i + d - 1)])
-            if(i + d < m)
-                D[(i + d):m, (i + d):m] .= Σ[(i + d):m, (i + d):m] 
-            end
+                L[(i + d):m, i:(i + d - 1)] * D[j] * transpose(L[(i + d):m, i:(i + d - 1)])
+            # if(i + d < m)
+            #     D[(i + d):m, (i + d):m] .= Σ[(i + d):m, (i + d):m]
+            #     D[i:i+d-1, i+d:m] .= 0
+            #     D[i+d:m, i:i+d-1] .= 0
+            # end
         end
+        L = LowerTriangular(L)
         return (L, D)
     end
 end
@@ -36,14 +48,13 @@ function CMVN(Σ::Symmetric{T,Array{T,2}}, a::Array{T,1}, b::Array{T,1}, d::Int;
     s = Int(m / d)
     y = zeros(m)
     P = 1.
-    L, D = LDL(copy(Σ), m, d)
+    L, D = LDL(copy(Σ), d)
     for i in 1:s
         j = Int((i - 1) * d)
         g = L[(j + 1):(j + d), 1:j] * y[1:j]
         a1 = a[(j + 1):(j + d)] .- g
         b1 = b[(j + 1):(j + d)] .- g
-        D1 = D[(j + 1):(j + d), (j + 1):(j + d)]
-        
+        D1 = Symmetric(D[i])
         L1 = cholesky(D1).L
         # P *= cdf_trunnormal(a1, b1, zeros(d), copy(Symmetric(D1)))
         # y[(j + 1):(j + d)] .= ex_trunnormal(a1, b1, zeros(d), copy(Symmetric(D1)))
